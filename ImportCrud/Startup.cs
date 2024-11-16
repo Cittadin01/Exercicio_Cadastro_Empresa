f3aab460e5a6100a47327b0b39c3e8a793038b2f
Fabrício Pinto Ferreira <fabricio.pinto@narwalpartner.com.br>
2024-11-16 11:00:06 -0300

    chore: init project

---
+++ ImportCrud/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImportCrud.Startup))]
+namespace ImportCrud
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
