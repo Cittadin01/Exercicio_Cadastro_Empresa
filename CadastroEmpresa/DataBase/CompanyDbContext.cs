032c4411c1b3f58bd091f8ab4a29c10883c58739
Vinicius Cittadin <274@narwalsistemas.com.br>
2024-11-21 10:42:38 -0300

    Banco de dados

---
+++ CadastroEmpresa/DataBase/CompanyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CadastroEmpresa.Models.Company;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CadastroEmpresa.DataBase
+{
+    public class CompanyDbContext : 
+    {
+        public CompanyDbContext() : base("")
+        {
+        }
+
+        public class DbSet<Company> Company { get; set; }
+        public object Conventions { get; private set; }
+    }
+}
---
+++ CadastroEmpresa/DataBase/CompanyInitializer.cs
@@ -0,0 +1,40 @@
+﻿using CadastroEmpresa.DataBase;
+using CadastroEmpresa.Models;
+using CadastroEmpresa.Models.Company;
+using System;
+using System.Collections.Generic;
+
+namespace CadastroEmpresa.Database
+{
+    public class CompanyInitializer
+    {
+        public void Seed(CompanyDbContext context)
+        {
+            var company = new List<Company>
+            {
+                new Company
+                {
+                    Id = 1,
+                    Cnpj = "63095677000170",
+                    Nome = "Empresa Teste",
+                    País = "Brazil",
+                    Endereço = "Rua 123",
+                    Numero = 789,
+                    Cep = 88840033,
+                    Active = true,
+                },
+                new Company
+                {
+                    Id = 2,
+                    Cnpj = "12395417000152",
+                    Nome = "Narwal",
+                    País = "Brazil",
+                    Endereço = "Avenida Teste",
+                    Numero = 528,
+                    Cep = 88810022,
+                    Active = true,
+                }
+            };
+        }
+    }
+}
